6f1d0f1a623c7280b539981735e705997f42079e
shivamngpal <shivamnagpal2005@gmail.com>
2025-12-29 06:00:01 +0530

    changing /health route from POST endpoint to GET endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,6 +5,6 @@ const protectRoute = require("../middleware/authMiddleware.js");
 const router = express.Router();
 router.post("/signup",signup);
 router.post("/login",login);
-router.post("/health",protectRoute,healthCheck);
+router.get("/health",protectRoute,healthCheck);
 
 module.exports =  router;
